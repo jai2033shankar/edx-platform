e28cb4afc8a4aec4bd00d3618e667926e9671168
wajeeha-khalid <wajeeha.khalid@arbisoft.com>
2016-07-26 12:38:33 +0500

    addressed feedback and remove LMS_ROOT_URL from common.py

--- common/djangoapps/startup_configurations/validate_config.py
+++ common/djangoapps/startup_configurations/validate_config.py
@@ -1,38 +1,37 @@
 """
 Common Functions to Validate Configurations
 """
-from django.conf import settings
 
 
-def validate_lms_config():
+def validate_lms_config(settings):
     """
     Validates configurations for lms and raise ValueError if not valid
     """
-    validate_common_config()
+    validate_common_config(settings)
 
     # validate feature based configurations
-    validate_marketing_site_config()
+    validate_marketing_site_config(settings)
 
 
-def validate_cms_config():
+def validate_cms_config(settings):
     """
     Validates configurations for lms and raise ValueError if not valid
     """
-    validate_common_config()
+    validate_common_config(settings)
 
     # validate feature based configurations
-    validate_marketing_site_config()
+    validate_marketing_site_config(settings)
 
 
-def validate_common_config():
+def validate_common_config(settings):
     """
     Validates configurations common for all apps
     """
-    if not settings.LMS_ROOT_URL:
+    if not getattr(settings, 'LMS_ROOT_URL', None):
         raise ValueError("'LMS_ROOT_URL' is not defined.")
 
 
-def validate_marketing_site_config():
+def validate_marketing_site_config(settings):
     """
     Validates 'marketing site' related configurations
     """
@@ -40,4 +39,4 @@ def validate_marketing_site_config():
         if not hasattr(settings, 'MKTG_URLS'):
             raise ValueError("'ENABLE_MKTG_SITE' is True, but 'MKTG_URLS' is not defined.")
         if not settings.MKTG_URLS.get('ROOT'):
-            raise ValueError("There is no 'ROOT' defined in 'MKTG_URLS'")
+            raise ValueError("There is no 'ROOT' defined in 'MKTG_URLS'.")
